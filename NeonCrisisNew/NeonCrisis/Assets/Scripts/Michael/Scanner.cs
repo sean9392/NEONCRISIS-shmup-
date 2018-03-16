9a6b72a2d0b0ba2a8eb9c2360c09e8c556f1209e
sean9392 <seancls@hotmail.co.uk>
2018-03-16 14:57:46 +0000

    ! BOSS ASS BITCH

---
+++ NeonCrisisNew/NeonCrisis/Assets/Scripts/Michael/Scanner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scanner : MonoBehaviour {
+
+	[SerializeField]
+	private Image scannerImage;
+
+	private float rot;
+
+	[SerializeField]
+	private RectTransform rect;
+
+	[SerializeField]
+	private Sprite boss;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		rect.rotation = Quaternion.Lerp (rect.rotation, Quaternion.Euler (new Vector3 (0, 0, rot)), Time.deltaTime * 2);
+
+		if (GameObject.Find ("Boss(Clone)")) 
+		{
+			scannerImage.sprite = boss;
+			scannerImage.material = null;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		
+		if (coll.gameObject.GetComponent<Enemy_base> ()) 
+		{
+			scannerImage.sprite = coll.gameObject.GetComponent<SpriteRenderer> ().sprite;
+			scannerImage.material = coll.gameObject.GetComponent<SpriteRenderer> ().material;
+			rot += 90;
+		}
+
+	}
+
+
+	void OnTriggerStay2D(Collider2D coll)
+	{
+		if (coll.gameObject.GetComponent<Boss> ()) 
+		{
+			
+
+		}
+	}
+}
